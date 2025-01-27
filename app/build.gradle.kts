9dc939ff2b797dc27e3275184aca215b46cad6e4
Dennis Titze <dennis.titze@guardsquare.com>
2025-01-27 16:15:48 +0100

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
